37628804285dd83bd78748f7fcb06f2b90e294e0
Piyush Sharma <89131804+Piyush-Sh-bit@users.noreply.github.com>
2025-07-12 14:57:01 +0530

    Delete python Full Stack/Django weekend assignment level 1 directory

--- python Full Stack/Django weekend assignment level 1/core_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('courses/', include('courses.urls')),
-    path('jobs/', include('jobs.urls')),
-    path('patients/', include('patients.urls')),
-]
